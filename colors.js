61081447410b0daef3fa02c135e3832de346caae
TelAr <qet112313@gmail.com>
2021-08-17 23:02:54 +0900

    add new project
    avoider

--- colors.js
+++ colors.js
@@ -1,24 +1,21 @@
 
-  function switchNightDay(self)
-  {
+  function switchNightDay(self) {
     var body_style=document.querySelector('body').style;
     var allLink=document.querySelectorAll('a');
     var link_color;
-    if(self.value ==='Night')
-    {
+    if(self.value ==='Night') {
       body_style.backgroundColor ='black';
       body_style.color ='#bbbbbb';
       link_color='gray';
       self.value='Day';
     }
-    else
-    {
+    else {
       body_style.backgroundColor ='white';
       body_style.color ='black';
       link_color='black';
       self.value='Night';
     }
-    for(var i=0;i<allLink.length;i++){
+    for(var i=0;i<allLink.length;i++) {
       allLink[i].style.color=link_color;
     }
     document.querySelector('.now').style.color ='#bbbbbb';
